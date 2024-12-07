835b8e912ad7aa9466837a6cbcf69e16bf668f63
dracudev <andreujavier99@gmail.com>
2024-12-07 13:12:07 +0100

    hover transitions for social icons and nav items

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,14 +9,49 @@ export default function Navbar() {
     <nav className="flex w-full flex-col items-center justify-center">
       <div className="flex w-full max-w-4xl flex-col items-center justify-between px-4 py-6 sm:flex-row">
         <div className="order-3 mt-6 flex flex-1 justify-start space-x-3 sm:order-1 sm:mt-0">
-          <FaTiktok />
-          <FaInstagram />
-          <FaXTwitter />
-          <FaFacebook />
-          <FaYoutube />
+          <a
+            href="https://www.tiktok.com/@starwars"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="transition-colors duration-300 hover:text-orange-700"
+          >
+            <FaTiktok />
+          </a>
+          <a
+            href="https://www.instagram.com/starwars/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="transition-colors duration-300 hover:text-orange-700"
+          >
+            <FaInstagram />
+          </a>
+          <a
+            href="https://x.com/starwars"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="transition-colors duration-300 hover:text-orange-700"
+          >
+            <FaXTwitter />
+          </a>
+          <a
+            href="https://www.facebook.com/starwars.es/?brand_redir=169299103121699"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="transition-colors duration-300 hover:text-orange-700"
+          >
+            <FaFacebook />
+          </a>
+          <a
+            href="https://www.youtube.com/user/starwars"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="transition-colors duration-300 hover:text-orange-700"
+          >
+            <FaYoutube />
+          </a>
         </div>
-        <img src={logo} width={200} className="mx-auto" />
-        <div className="mt-6 flex flex-1 justify-end sm:m-0">
+        <img src={logo} width={200} className="mx-auto sm:order-2" />
+        <div className="mt-6 flex flex-1 justify-end sm:order-3 sm:m-0">
           <img src={login} className="mb-[1.9px] me-2" />
           <p className="flex items-center justify-center">LOG IN</p>
         </div>
@@ -25,12 +60,12 @@ export default function Navbar() {
       <div className="flex h-12 w-full items-center justify-center border-y-2 border-solid border-y-zinc-800">
         <ul className="flex h-full w-full items-center justify-center">
           <Link to="/" className="h-full">
-            <li className="flex h-full items-center justify-center border-l-2 border-solid border-l-zinc-800 px-4 py-2 hover:border-b-[2px] hover:border-b-orange-700">
+            <li className="flex h-full items-center justify-center border-l-2 border-solid border-b-orange-700 border-l-zinc-800 px-4 py-2 transition-colors duration-300 hover:h-[46px] hover:border-b-[2px] hover:border-b-orange-700 hover:text-orange-700">
               HOME
             </li>
           </Link>
           <Link to="/starships" className="h-full">
-            <li className="flex h-full items-center justify-center border-x-2 border-solid border-x-zinc-800 px-4 py-2 hover:border-b-[2px] hover:border-b-orange-700">
+            <li className="flex h-full items-center justify-center border-x-2 border-solid border-x-zinc-800 border-b-orange-700 px-4 py-2 transition-colors duration-300 hover:h-[46px] hover:border-b-[2px] hover:border-b-orange-700 hover:text-orange-700">
               STARSHIPS
             </li>
           </Link>
