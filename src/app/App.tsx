aef7e16e5f8cea5f7505aab8656d5988fcb4262d
dracudev <andreujavier99@gmail.com>
2025-08-09 12:18:06 +0200

    chore: disable protected

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,13 +6,13 @@ import Home from "./pages/Home";
 import { Footer } from "../components/Footer";
 import Login from "./pages/auth/Login";
 import SignUp from "./pages/auth/SignUp";
-import ProtectedRoute from "../components/utils/ProtectedRoute";
-import { useSelector } from "react-redux";
-import { RootState } from "../redux/store";
+// import ProtectedRoute from "../components/utils/ProtectedRoute";
+// import { useSelector } from "react-redux";
+// import { RootState } from "../redux/store";
 import Details from "./pages/Details";
 
 function App() {
-  const user = useSelector((state: RootState) => state.user);
+  // const user = useSelector((state: RootState) => state.user);
 
   return (
     <Router>
@@ -21,17 +21,18 @@ function App() {
         <div className="flex flex-grow items-center justify-center bg-[url('./assets/bg.webp')] p-4">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route
+            {/*             <Route
               element={
                 <ProtectedRoute
                   canActivate={user.isAuthenticated}
                   redirectPath="/login"
                 />
               }
-            >
-              <Route path="/starships" element={<Starships />} />
-              <Route path="/starships/:id" element={<Details />} />
-            </Route>
+            > */}{" "}
+            {/* Disabled protected routes for demo */}
+            <Route path="/starships" element={<Starships />} />
+            <Route path="/starships/:id" element={<Details />} />
+            {/*             </Route> */}
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<SignUp />} />
           </Routes>
